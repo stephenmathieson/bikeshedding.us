f31f45f891f3faf0bd3e84f514ac41424ead72ad
Stephen Mathieson <me@stephenmathieson.com>
2017-04-13 13:09:41 -0700

    Header: prevent navigation clicks from bubbling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { Link } from 'react-router'
 import styles from './Header.css'
 
+// a HeaderLink prevents the `click` from bubbling, which will
+// allow navigation without changing the App bg color
+const HeaderLink = props => (
+  <Link to={props.to} onClick={e => e.stopPropagation()}>{props.children}</Link>
+)
+
 const Header = props => (
   <header className={styles.header}>
     <h1>Bike Shedding · us</h1>
     <nav className={styles.navigation}>
       <ul>
-        <li><Link to='/'>/</Link></li>
-        <li><Link to='/email'>/email</Link></li>
+        <li><HeaderLink to='/'>/</HeaderLink></li>
+        <li><HeaderLink to='/email'>/email</HeaderLink></li>
       </ul>
     </nav>
   </header>
